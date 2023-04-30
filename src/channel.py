1f953cbb675eccdab3c01fa2ab85aec3a29fc225
dasha <dsgurieva@gmail.com>
2023-04-30 10:16:36 +0500

    New commit 3

--- src/channel.py
+++ src/channel.py
@@ -8,6 +8,7 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 class Channel:
     """Класс для ютуб-канала"""
 
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
@@ -19,44 +20,56 @@ class Channel:
         self.description = self.channel['items'][0]['snippet']['description']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
 
+
     def __str__(self):
         """Возвращает название и ссылку на канал"""
         return f'{self.title} ({self.url})'
+
 
     def __add__(self, other):
         """Реализует возможность сложения по количеству подписчиков"""
         return int(self.subscriber_count) + int(other.subscriber_count)
 
+
     def __sub__(self, other):
         """Реализует возможность вычитания по количеству подписчиков"""
         return int(self.subscriber_count) - int(other.subscriber_count)
+
 
     def __gt__(self, other):
         """Сравнивает количество подписчиков self>other по количеству подписчиков"""
         return int(self.subscriber_count) > int(other.subscriber_count)
 
+
     def __ge__(self, other):
         """Сравнивает количество подписчиков self>=other по количеству подписчиков"""
         return int(self.subscriber_count) >= int(other.subscriber_count)
+
 
     def __lt__(self, other):
         """Сравнивает количество подписчиков self<other по количеству подписчиков"""
         return int(self.subscriber_count) < int(other.subscriber_count)
 
+
     def __le__(self, other):
         """Сравнивает количество подписчиков self<=other по количеству подписчиков"""
         return int(self.subscriber_count) <= int(other.subscriber_count)
+
+
     def __eq__(self, other):
         """Сравнивает количество подписчиков self==other по количеству подписчиков"""
         return int(self.subscriber_count) == int(other.subscriber_count)
+
 
     @property
     def channel_id(self) -> str:
         return self.__channel_id
 
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         return self.channel
+
 
     def to_json(self, path):
         """Метод, сохраняющий в файл значения атрибутов экземпляра Channel"""
@@ -71,6 +84,7 @@ class Channel:
                 'url': self.url
 
             }, file, indent=2, ensure_ascii=False)
+
 
     @classmethod
     def get_service(cls):
