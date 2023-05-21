cfed598c154f04bbb5e87b4ca64ccdf615e877da
dasha <dsgurieva@gmail.com>
2023-05-21 12:06:55 +0500

    Home work 6, добавление исключений

--- src/video.py
+++ src/video.py
@@ -1,6 +1,6 @@
 import os
 from googleapiclient.discovery import build
-
+from googleapiclient.errors import HttpError
 
 api_key: str = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -8,16 +8,39 @@ class Video:
 
 
     def __init__(self, id_video):
-        self.id_video = id_video
-        self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=self.id_video).execute()
-        self.url = f"https://www.youtube.com/watch?v={self.id_video}"
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
+        self.id_video = id_video
+
+        try:
+            self.url = f"https://www.youtube.com/watch?v={self.id_video}"
+            self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=self.id_video).execute()
+
+        except HttpError:
+            self.url = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+
+        try:
+             if not self.video_response["title"]:
+                raise KeyError("Ошибка индекса")
+        except KeyError:
+            self.url = None
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+
+        else:
+            self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=self.id_video).execute()
+            self.url = f"https://www.youtube.com/watch?v={self.id_video}"
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 class PLVideo(Video):
 
